4f761de239215d1a790d8efbc313bd42dde18b02
azhiltsov <azhiltsov@gmail.com>
2018-05-06 13:46:41 +0700

    create role and roleBinding

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 1. use "dep" dependency manager to get proper version of used client go and its transitive dependencies like: `dep ensure`
 2. For authorization used config with certificates, in this case its **minikube** username
-3. for authentication CA cert is needed in Windows its in `{minikube_home}/.minikube/ca.crt`
+
+How program works
+=============
+1.  To run program user should provide path to kubernetes config
+`SomeApp.exe -kubeconfig=C:\Users\Wizzard\.kube\config`
+
+2.  program run several steps, every step in namespace `default`:
+
+    2.1. It shows all roleBinding objects for namespace
+     
+    2.2. It creates role with name `deployment-manager`
+    
+    2.3. It binds role to user with name `someEmployee`
+    
+    2.4. It shows all roleBinding again to indicate that role was binded    
+
+3. after that, to use this `someEmployee` and its role you need:
+
+    3.1. create certificates and sign it with kubernetes CA cert, **NOTE** that CN= name should be same as user name in RoleBinding!
+                     
+          openssl.exe genrsa -out someEmployee.key 2048
+          openssl.exe req -new -key someEmployee.key -out someEmployee.csr -subj "/CN=someEmployee/O=test"
+          openssl.exe x509 -req -in someEmployee.csr -CA ./ca.crt -CAkey ./ca.key -CAcreateserial -out someEmployee.crt -days 500
+    3.2. create user specific context in kubectl config
+          
+          kubectl config set-credentials employee --client-certificate=./someEmployee.crt  --client-key=./someEmployee.key
+          kubectl config set-context some-employee-context --cluster=minikube --namespace=default --user=employee
+          kubectl --context=some-employee-context get pods
+          
+            
+                  
--- SomeApp.go
+++ SomeApp.go
@@ -8,9 +8,10 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/api/rbac/v1"
 )
 
-// optional - local kubeconfig for testing
+// local kubeconfig for testing
 var kubeconfig = flag.String("kubeconfig", "", "Path to a kubeconfig file")
 
 func main() {
@@ -19,8 +20,12 @@ func main() {
 	flag.Set("logtostderr", "true")
 	flag.Set("v", "3")
 	flag.Parse()
+	if (*kubeconfig == "") {
+		glog.Errorf("Please provide path to config  -kubeconfig=[path]")
+		return
+	}
 
-	config, err := getConfig("C:/Users/Wizzard/.kube/config")
+	config, err := getConfig(*kubeconfig)
 	if err != nil {
 		glog.Errorf("Failed to load client config: %v", err)
 		return
@@ -33,26 +38,22 @@ func main() {
 		return
 	}
 
-	getSomething(client)
+	printRoleBinding(client)
+	role := createDeploymentRole()
+	createRoleOnKub(client, role)
+	bindRole(client, role, "someEmployee")
+	printRoleBinding(client)
 
-	/*// list pods
-	pods, err := client.CoreV1().Nodes().List(metav1.ListOptions{})
-	if err != nil {
-		glog.Errorf("Failed to retrieve pods: %v", err)
-		return
-	}
 
-	for _, p := range pods.Items {
-		glog.V(3).Infof("Found pods: %s/%s", p.Namespace, p.Name)
-	}*/
 }
 
 func getConfig(kubeconfig string) (*rest.Config, error) {
 	return clientcmd.BuildConfigFromFlags("", kubeconfig)
 }
 
-func getSomething(client *kubernetes.Clientset)  () {
-	bindings, err := client.RbacV1().ClusterRoleBindings().List(metav1.ListOptions{})
+/*print list of role bindings*/
+func printRoleBinding(client *kubernetes.Clientset)  () {
+	bindings, err := client.RbacV1().RoleBindings("default").List(metav1.ListOptions{})
 	if err != nil {
 		glog.Errorf("Failed to get binding list: %v", err)
 		return
@@ -63,24 +64,61 @@ func getSomething(client *kubernetes.Clientset)  () {
 
 }
 
-/*func GetInClusterConfig() (*rest.Config, error) {
-	host := "192.168.0.149"
-	port := "8443"
+/*Create role with parameters on Kubernetes*/
+func createRoleOnKub(client *kubernetes.Clientset, role *v1.Role) (*v1.Role){
 
-	token := "eyJhbGciOiJSUzI1NiIsImtpZCI6IiJ9.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJkZWZhdWx0Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZWNyZXQubmFtZSI6ImRlZmF1bHQtdG9rZW4tNDJsYjkiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC5uYW1lIjoiZGVmYXVsdCIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50LnVpZCI6ImUyZTE3MjQ2LTUwNGMtMTFlOC05NDA3LTAwMTU1ZDAwMmEwMyIsInN1YiI6InN5c3RlbTpzZXJ2aWNlYWNjb3VudDpkZWZhdWx0OmRlZmF1bHQifQ.UMgBhV6aGFnLIcY8MhcGsnaTpJPji5VvtFI_lSd9RxNwNAvIQN4WKS92l_ryaaoKbQkqlWbXCc5HEXYQ4OBoZEpgoPXvSd6eujn1td-WLSXLVwvhwYUudg8Y5YgsL4M0k9vewftm9SOFYh2v5MsfhKpcaXOeWmJ0axtxJHcKUvuhiwjb6ZO_a8aCzmoiJhZL0gbtKP7PqBy4XjRNkamqEVDudRgoVR4fbF_CdwfDoKK9WS2oq_ocLD5UP_cqZOezwBzzoLMjNtDfJ7LzuOT5Wz6P78Ll-tyOro36UdAtN8uqNkVn3vXKRXTOzAAL37blDc7ISrod_vGm3I2kZ5R-EA"
-	tlsClientConfig := rest.TLSClientConfig{}
-	rootCAFile := "C:/minikube_home/.minikube/ca.crt"
-	if _, err := certutil.NewPool(rootCAFile); err != nil {
-		glog.Errorf("Expected to load root CA config from %s, but got err: %v", rootCAFile, err)
-	} else {
-		tlsClientConfig.CAFile = rootCAFile
+	roleCreationResult, err := client.RbacV1().Roles("default").Create(role)
+	if err != nil {
+		glog.Errorf("Failed to create role: %v", err)
+		return nil
+	}
+	return roleCreationResult
+}
+
+/*Create role structure which will be created on kubernetes*/
+func createDeploymentRole() (*v1.Role) {
+	rule := v1.PolicyRule{
+		Verbs:           []string{"get", "list", "watch", "create", "update", "patch", "delete"},
+		APIGroups:       []string{"", "extensions", "apps"},
+		Resources:       []string{"deployments", "replicasets", "pods"},
 	}
 
-	return &rest.Config{
-		// TODO: switch to using cluster DNS.
-		Host:            "https://" + net.JoinHostPort(host, port),
-		BearerToken:     string(token),
-		TLSClientConfig: tlsClientConfig,
-	}, nil
-}*/
+	return &v1.Role{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "",
+			APIVersion: "",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "deployment-manager",
+			Namespace: "default",
+		},
+		Rules: []v1.PolicyRule{rule},
+	}
+}
+
+/*Bind some Role which is already on Kubernetes to user name provided*/
+func bindRole(client *kubernetes.Clientset, role *v1.Role, userName string) {
+	subject := v1.Subject{
+		Kind:      "User",
+		APIGroup:  "",
+		Name:      userName,
+		Namespace: "default",
+	}
+
+	_, err := client.RbacV1().RoleBindings("default").Create(&v1.RoleBinding{
+
+		ObjectMeta: metav1.ObjectMeta{
+			Name: role.Name + "-binding",
+		},
+		Subjects: []v1.Subject{subject},
+		RoleRef: v1.RoleRef{
+			APIGroup: "",
+			Kind:     "Role",
+			Name:     role.Name,
+		},
+	})
+	if err != nil {
+		glog.Errorf("Failed to bind role: %v", err)
+	}
+}
 
